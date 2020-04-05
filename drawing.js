b22808b182864f1d42bd2127ecd5530a725294ab
grhoward110 <60433954+grhoward110@users.noreply.github.com>
2020-04-05 12:59:39 -0400

    drawing.js updateed the drawing ghosts capability, objects.js has constructors for pacman and ghosts, and pacman3.html combines both javascript files.
    This new code may not work. Review by other branches.

--- drawing.js
+++ drawing.js
@@ -1,5 +1,6 @@
 // Concepts and codes taught by Gaeme Stuart from Introduction to JavaScript Game Development
 function draw_grid(ctx, minor, major, stroke, fill) {
+	
 	minor = minor || 10; 
 	
 	major = major || minor * 5; 
@@ -14,40 +15,98 @@ function draw_grid(ctx, minor, major, stroke, fill) {
 	
 	ctx.fillStyle = fill; 
 	
-	let width = ctx.canvas.width, height = ctx.canvas.height
-	
-	for(var x = 0; x < width; x += minor) {
+	for(var x = 0; x < ctx.canvas.width; x += minor) {
 		
 		ctx.beginPath(); 
 		
 		ctx.moveTo(x, 0); 
 		
-		ctx.lineTo(x, height); 
+		ctx.lineTo(x, ctx.canvas.height); 
 		
 		ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25; 
 		
 		ctx.stroke(); 
 		
-		if(x % major == 0 ) {ctx.fillText(x, x, 10);}
+		if(x % major == 0) {ctx.fillText(x, x, 10);} 
 	}
 	
-	for(var y = 0; y < height; y += minor) {
+	for(var y = 0; y < ctx.canvas.height; y += minor) {
 		
 		ctx.beginPath(); 
 		
 		ctx.moveTo(0, y); 
 		
-		ctx.lineTo(width, y); 
+		ctx.lineTo(ctx.canvas.width, y);
 		
-		ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+		ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25; 
 		
 		ctx.stroke(); 
 		
-		if(y % major == 0 ) {ctx.fillText(y, 0, y+ 10);}
+		if(y % major == 0) {ctx.fillText(y, 0, y + 10);} 
 	}
+	
+}
+
+function draw_pacman(context, x, y, radius, mouth) {
+	
+	angle = 0.2 * Math.PI * mouth; 
+	
+	context.save(); 
+	
+	context.fillStyle = "yellow"; 
+	
+	context.strokeStyle = "black"; 
+	
+	context.beginPath(); 
+	
+	context.arc(x, y, radius, angle, -angle); 
+	
+	context.lineTo(x, y); 
+	
+	context.stroke();
+	
+	context.fill();
+	
+	context.closePath(); 
+	
+	context.restore(); 
+}
+
+function draw_ghost(ctx, radius, options) {
+	
+	options = options || {}; 
+	
+	var feet = options.feet || 4; 
+	
+	var head_radius = radius * 0.8; 
+	
+	var foot_radius = head_radius / feet; 
+	
+	ctx.save(); 
+	
+	ctx.strokeStyle = options.stroke || "white"; 
+	
+	ctx.fillStyle = options.fill || "red"; 
+	
+	ctx.lineWidth = options.lineWidth || radius * 0.05; 
+	
+	ctx.beginPath(); 
+	
+	for(foot = 0; foot < feet; foot++) {
+		
+		ctx.arc(2 * foot_radius * (feet - foot)) - head_radius - foot_radius, radius - foot_radius, foot_radius, 0, Math.PI); 
+		
+	}
+	
+	ctx.lineTo(-head_radius, radius - foot_radius); 
+	
+	ctx.arC(0, head_radius - radius, head_radius, Math.PI, 2 * Math.PI); 
+	
+	ctx.closePath(); 
+	
+	ctx.fill(); 
+	
+	ctx.stroke(); 
 	
 	ctx.restore(); 
 }
-
-
-			
